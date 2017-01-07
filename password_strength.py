1f3cdeb2b174846bf916948d3ecf42a9bf7818c1
andrey.selemenev@rtlabs.ru <andrey.selemenev@rtlabs.ru>
2017-01-08 00:36:43 +0300

    use enum, refactoring

--- password_strength.py
+++ password_strength.py
@@ -1,36 +1,32 @@
 import argparse
 import getpass
 import re
+from enum import Enum
 
-global black_list
 
-class PasswordErrorsEnum():
-    offensive_words = 'Не используйте запрещенные слова, и личную информацию в пароле'
-    mixed_case = 'Используйте разный регистр букв'
-    digits = 'Пароль не содержит цифр'
-    length = 'Используйте длинные пароли, не менее 8 символов'
-    special_chars = 'Используйте специальные символы в пароле'
+class PasswordErrorsEnum(Enum):
+    mixed_case = 1
+    digits = 2
+    special_chars = 3
 
+
+password_errors_map = {
+    PasswordErrorsEnum.mixed_case: 'Используйте разный регистр букв',
+    PasswordErrorsEnum.digits: 'Пароль не содержит цифр',
+    PasswordErrorsEnum.special_chars: 'Используйте специальные символы в пароле'
+}
 
 def get_password_strength(password):
     strength = 1
     recomendations = []
-    if __has_offensive_words(password):
-        recomendations.append(PasswordErrorsEnum.offensive_words)
-        return strength, recomendations
-    strength = 0
-    if __is_case_sensitive(password):
-        strength += 2
-    else:
-        recomendations.append(PasswordErrorsEnum.mixed_case)
     if __has_number(password):
         strength += 1
     else:
         recomendations.append(PasswordErrorsEnum.digits)
-    if __is_valid_password_length(password):
+    if __is_case_sensitive(password):
         strength += 2
     else:
-        recomendations.append(PasswordErrorsEnum.length)
+        recomendations.append(PasswordErrorsEnum.mixed_case)
     if __has_special_chars(password):
         strength += 5
     else:
@@ -47,31 +43,14 @@ def __has_special_chars(password):
     patter_str = r'[`~!@#$%^&*()_{}[]|\+-:;"<>,.?]'
     return bool(re.findall(patter_str, password))
 
-def __read_list_from_file(filestream):
-    with filestream as f:
-        return [line.rstrip('\n') for line in f.readlines()]
-
-def __has_offensive_words(password):
-    if not black_list:
-        return False
-    pattern = re.compile('(' + '|'.join(black_list) + ')', re.IGNORECASE)
-    return bool(pattern.findall(password))
-
-def __is_valid_password_length(password, length=8):
-    return len(password) >= length
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Check password strength")
-    parser.add_argument("-b", "--black-list", type=argparse.FileType('r'), dest="blacklist")
-    parser.add_argument("-l", "--length", type=int, default=8, help='minimum password length')
     options = parser.parse_args()
-    if options.blacklist:
-        black_list = __read_list_from_file(options.blacklist)
     password = getpass.getpass('Enter password:')
     strength, recomendations = get_password_strength(password)
     print('Оценка сложности пароля (макс. 10): {0}'.format(strength))
     if recomendations:
         print('Рекомендации по повышению взломостойкости пароля:')
         for recom in recomendations:
-            print('\t{0}'.format(recom))
+            print('\t{0}'.format(password_errors_map.get(recom)))
